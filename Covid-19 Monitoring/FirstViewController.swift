04bdb25c0e7e7a601a9527e7e754510466a4ebc6
Mohamed <m-j-alghamdi@hotmail.com>
2020-04-23 17:51:29 +0300

    Initial Commit

---
+++ Covid-19 Monitoring/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Covid-19 Monitoring
+//
+//  Created by mohamed al-ghamdi on 23/04/2020.
+//  Copyright © 2020 mohamed al-ghamdi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
